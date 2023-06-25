57c7699b59e0c767d154840ecb08bd6eb5839aed
Hitesh <kumar.hiteshkumar40@gmail.com>
2023-06-25 12:04:43 +0530

    autosearch

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { HiOutlineSearch } from "react-icons/hi";
 import "./style.scss";
 
+import { fetchDataFromApi } from "../../../utils/api";
 import useFetch from "../../../hooks/useFetch";
 import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
@@ -13,8 +15,10 @@ const HeroBanner = () => {
   const navigate = useNavigate();
   const url = useSelector((state) => state.home.url);
   const [searchError, setSearchError] = useState("");
-
   const { data, loading } = useFetch("/movie/upcoming");
+  const [showSuggestion, setShowSuggestion] = useState("hide");
+  const [showSuggestionResults, setShowSuggestionResults] = useState(null);
+  const inputSearchRef = useRef();
 
   // console.log(searchError);
 
@@ -25,19 +29,89 @@ const HeroBanner = () => {
     setBackground(bg);
   }, [data]);
 
-  const searchInputChangeHandler = (event) => {
-    setQuery(event.target.value);
+  const fetchSearchData = () => {
+    fetchDataFromApi(
+      `/search/multi?query=${inputSearchRef.current.value}&page=1`
+    )
+      .then((res) => {
+        const response = [];
+        for (let i = 0; i < res.results.length; i++) {
+          const str = res.results[i].title || res.results[i].name;
+          if (!response.includes(str)) {
+            response.push(str);
+          }
+          if (response.length >= 5) {
+            break;
+          }
+        }
+        setShowSuggestionResults(response);
+        // console.log(response);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const outcome = (data) => {
+    setQuery({ data });
     setSearchError("");
+    if (inputSearchRef.current.value !== "") {
+      // console.log("output from the server....", data);
+      setShowSuggestionResults(null);
+      setShowSuggestion("");
+      fetchSearchData();
+    }
+  };
+
+  const debounce = (func, timeOut) => {
+    let timer;
+
+    return (...args) => {
+      if (timer) {
+        // console.log("clearing the timer");
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => {
+        func(args);
+      }, timeOut);
+    };
+  };
+
+  const result = debounce(outcome, 1000);
+
+  const searchInputChangeHandler = (event) => {
+    // console.log(event.target.value);
+    if (event.target.value !== "") {
+      result(event.target.value);
+    }
+    //  setQuery(event.target.value);
+    //  setSearchError("");
   };
 
   const searchQueryHandler = (event) => {
     // console.log(event.key, query.length);
-    if ((event.key === "Enter" || event.type === "click") && query.length > 0) {
-      navigate(`/search/${query}`);
+    if (event.target.value !== "" && event.target.value.length > 0) {
       setSearchError("");
-    } else if (query.length <= 0) {
-      setSearchError("error");
     }
+    if (
+      (event.key === "Enter" && event.target.value.length > 0) ||
+      (event.type === "click" && inputSearchRef.current.value !== "")
+    ) {
+      // navigate(`/search/${event.target.value}`);
+      navigate(`/search/${inputSearchRef.current.value}`);
+
+      setSearchError("");
+    } else if (event.target.value <= 0) {
+      setSearchError("error");
+      setShowSuggestion("hide");
+    }
+  };
+
+  const setSuggestedResultHandler = (event) => {
+    setShowSuggestion("hide");
+    setShowSuggestionResults(null);
+    inputSearchRef.current.value = event.target.innerText;
   };
 
   return (
@@ -57,6 +131,7 @@ const HeroBanner = () => {
           </span>
           <div className={`searchInput ${searchError}`}>
             <input
+              ref={inputSearchRef}
               type="text"
               placeholder="Search for movie or TV show...."
               onChange={searchInputChangeHandler}
@@ -64,6 +139,20 @@ const HeroBanner = () => {
             />
             <button onClick={searchQueryHandler}>Search</button>
           </div>
+          {/* {showSuggestion && ( */}
+          <div className={`searchResults ${showSuggestion}`}>
+            {showSuggestionResults?.map((item) => (
+              <div
+                key={item}
+                className="searchItem"
+                onClick={setSuggestedResultHandler}
+              >
+                <HiOutlineSearch />
+                {item}
+              </div>
+            ))}
+          </div>
+          {/* )} */}
         </div>
       </ContentWrapper>
     </div>
